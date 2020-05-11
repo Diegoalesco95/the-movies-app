f21cd6d3d7fdab464bb473ec1dd184f00f409870
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-05-11 18:59:05 -0500

    Separators & empty lists

---
+++ src/main/components/VerticalSeparator/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {styles} from './styles';
+
+export const VerticalSeparator = ({color}) => {
+  return (
+    <View
+      style={[styles.separator, {borderTopColor: color ? color : '#a2b4bc'}]}>
+      <Text>Esto es un separador</Text>
+    </View>
+  );
+};
--- src/main/containers/SuggestionList.js
+++ src/main/containers/SuggestionList.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import {FlatList, Text} from 'react-native';
 import {SuggestionListComponent} from '../components/SuggestionList';
+import {Empty} from '../components/Empty';
+import {VerticalSeparator} from '../components/VerticalSeparator';
 
 export const SuggestionList = () => {
-  const list = [{title: 'Titulo 1', key: '1'}, {title: 'Titulo 2', key: '2'}];
+  // const list = [];
+  const list = [
+    {title: 'Avengers', key: '1'},
+    {title: 'Mortal Kombat', key: '2'},
+  ];
   return (
     <SuggestionListComponent title="Recomendado para ti">
       <FlatList
         data={list}
+        ListEmptyComponent={() => <Empty text="No hay sugerencias ☹" />}
+        ItemSeparatorComponent={() => <VerticalSeparator />}
         renderItem={({item}) => <Text>{item.title}</Text>}
       />
     </SuggestionListComponent>
