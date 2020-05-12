6113c59e95c75876da72b2bfdd5730ff83189756
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-05-11 20:49:27 -0500

    Fetch data

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, StyleSheet} from 'react-native';
 import {Home} from './src/main/screens/Home';
 import {Header} from './src/main/components/Header';
 import {SuggestionList} from './src/main/containers/SuggestionList';
+import Api from './src/services/api';
 
 export default function App() {
+  const [movies, setMovies] = useState({});
+  useEffect(() => {
+    (async () => {
+      const movies = await Api.getSuggestion(10);
+      setMovies(movies);
+    })();
+  }, []);
+
+  console.log('Estas son las pelis', movies);
+
   return (
     <Home>
       <Header>
