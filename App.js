6a25f6fcc7330b532c09ebbc8ea497fe720f2891
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-05-18 14:53:03 -0500

    Config React-Navigation

--- App.js
+++ App.js
@@ -2,15 +2,23 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import {store, persistor} from './src/store/store';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import HomeLayout from './src/main/layouts/homeLayout';
 import {Loading} from './src/main/components/Loading';
+
+const RootStack = createStackNavigator();
 
 export default function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={<Loading />} persistor={persistor}>
-        <HomeLayout />
+        <NavigationContainer>
+          <RootStack.Navigator>
+            <RootStack.Screen name="Home" component={HomeLayout} />
+          </RootStack.Navigator>
+        </NavigationContainer>
       </PersistGate>
     </Provider>
   );
